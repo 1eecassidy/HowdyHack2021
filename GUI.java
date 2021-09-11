bcd9570aacee53da3028265d5f41e2c6122b736a
fluffypanda19 <82167303+fluffypanda19@users.noreply.github.com>
2021-09-11 16:22:14 -0500

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,61 @@
+package howdyHack2021;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI implements ActionListener{
+	
+	int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel; 
+	
+	public GUI() {
+		
+		frame = new JFrame();
+		
+		JButton button1 = new JButton("Harmonize");
+		button1.addActionListener(this);
+		
+		JButton button2 = new JButton("Triad");
+		button2.addActionListener(this);
+		
+		JButton button3 = new JButton("Random");
+		button3.addActionListener(this);
+		
+		label = new JLabel("Number of clicks: 0");
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(label);
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Our GUI");
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		new GUI();	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number of clicks: " + count);
+	}
+	
+}
